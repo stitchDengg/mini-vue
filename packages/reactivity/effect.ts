2a1b3c340bbcca9ef79d47f101c7569f066d9e89
邓浩 <1051766345@qq.com>
2024-04-30 00:01:43 +0800

    feat: 增加依赖收集和触发依赖

---
+++ packages/reactivity/effect.ts
@@ -0,0 +1,47 @@
+let activeEffect;
+
+class ReactiveEffect {
+  private _fn: Function;
+  constructor(fn) {
+    this._fn = fn;
+  }
+  run() {
+    activeEffect = this;
+    this._fn();
+  }
+}
+
+const targetMap = new WeakMap();
+
+export function track(target, key) {
+  // target => key => dep
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  dep.add(activeEffect);
+}
+
+export function trigger(target, key) {
+  const depsMap = targetMap.get(target);
+  if (!depsMap) return;
+  const dep = depsMap.get(key);
+  if (!dep) return;
+  dep.forEach((effect) => {
+    effect.run();
+  });
+}
+
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn);
+  // 需要一上来直接调用
+  _effect.run();
+}
---
+++ packages/reactivity/tests/reactive.spec.ts
@@ -0,0 +1,13 @@
+import { reactive } from "../reactive";
+
+describe("reactive", () => {
+  it("happy path", () => {
+    const original = { foo: 1 };
+    const observed = reactive(original);
+    // 两个对象绝对不想等
+    expect(observed).not.toBe(original);
+
+    // 期望observed.foo等于1
+    expect(observed.foo).toBe(1);
+  });
+});
