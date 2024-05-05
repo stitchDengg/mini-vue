7c5a7ee5d660b9a6872e7ae5a4e6440f487ee779
邓浩 <1051766345@qq.com>
2024-05-06 00:23:24 +0800

    feat: add stop and onStop event

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,16 +1,44 @@
-// 存
+import { extend } from "@mini-vue/shared/src";
+// 存储当前的收集依赖的实例
 let activeEffect;
 
+// 清除依赖
+function cleanupEffect(effect) {
+  effect.deps.filter((dep) => {
+    dep.delete(effect);
+  });
+}
+
 class ReactiveEffect {
+  /** 传入的函数 */
   private _fn: Function;
+  /** 实例状态，调用stop后为false */
+  private active = true;
+  /** 对应的所有依赖 */
+  public deps: any = [];
+  /** 是否传入调度器参数 */
   public scheduler?: Function;
-  constructor(fn, scheduler?) {
+  /** 传入的停止函数 */
+  public onStop?: Function;
+
+  constructor(fn) {
     this._fn = fn;
-    this.scheduler = scheduler;
   }
+
+  // 执行函数依赖
   run() {
     activeEffect = this;
     return this._fn();
+  }
+
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
   }
 }
 
@@ -31,7 +59,10 @@ export function track(target, key) {
     depsMap.set(key, dep);
   }
 
+  if (!activeEffect) return;
+
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
@@ -55,11 +86,25 @@ export function trigger(target, key) {
  * @param option 传入其他参数
  * @returns 返回一个函数runner，调用这个函数会执行fn
  */
-export function effect(fn, options?) {
-  const { scheduler } = options || {};
-  const _effect = new ReactiveEffect(fn, scheduler);
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn);
+
+  //extend
+  extend(_effect, options);
+
   // 需要一上来直接调用
   _effect.run();
 
-  return _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+/**
+ *
+ * @param runner 需要去停止的函数
+ */
+export function stop(runner) {
+  // 将这个函数从依赖中移除即可
+  runner.effect.stop();
 }
